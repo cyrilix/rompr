881d8ddae4b197d086246d8a1596c8992b3edbaa
fatg3rman <fatg3rman@94fc339e-1912-44e7-90be-6e134bb66470>
2012-01-04 13:03:19 +0000

    git-svn-id: http://svn.code.sf.net/p/rompr/code/trunk@26 94fc339e-1912-44e7-90be-6e134bb66470

---
+++ newplaylist.php
@@ -0,0 +1,39 @@
+<?php
+$playlist_type = $_POST['type'];
+$playlist = $_POST['xml'];
+
+//if (array_key_exists('stationurl', $_POST)) {
+    $playlist = preg_replace('/(<playlist.*?>)/', "$1\n<stationurl>".$_POST['stationurl']."</stationurl>", $playlist);
+//}
+
+$xml = simplexml_load_string($playlist, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+if($playlist_type == "radio") {
+    $title = $xml->playlist->title;
+    //error_log("New radio playlist with title ".$title);
+    if (file_exists('prefs/LFMRADIO_'.md5($title).'.xspf')) {
+        if (file_exists('prefs/OLD_LFMRADIO_'.md5($title).'.xspf')) {
+            unlink('prefs/OLD_LFMRADIO_'.md5($title).'.xspf');
+        }
+        system('mv prefs/LFMRADIO_'.md5($title).'.xspf prefs/OLD_LFMRADIO_'.md5($title).'.xspf');
+    }
+    $fp = fopen('prefs/LFMRADIO_'.md5($title).'.xspf', 'w');
+    if ($fp) {
+        fwrite($fp, $playlist);
+    }
+    fclose($fp);
+}
+
+if ($playlist_type == "track"){
+    $title = $xml->trackList->track->location;
+    //error_log("New Playlist for ".$title);
+    $fp = fopen('prefs/'.md5($title).'.xspf', 'w');
+    if ($fp) {
+        fwrite($fp, $playlist);
+    }
+    fclose($fp);
+}
+
+
+print "<html><body></body></html>";
+?>
