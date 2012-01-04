782cc977a0e7ffced4520bbb1f70b05e3ab250ab
fatg3rman <fatg3rman@94fc339e-1912-44e7-90be-6e134bb66470>
2012-01-04 13:02:17 +0000

    git-svn-id: http://svn.code.sf.net/p/rompr/code/trunk@22 94fc339e-1912-44e7-90be-6e134bb66470

---
+++ infocontrols.php
@@ -0,0 +1,17 @@
+
+<table width="100%">
+<tr>
+<td align="left">
+<a href="#" onclick="expandInfo('left')"><img id="expandleft" height="24px" src="images/arrow-left-double.png"></a>
+</td>
+<td align="center">
+<a href="#" onclick="browser.switchSource('wikipedia')"><img height="24px" src="images/Wikipedia-logo.png"></a>
+<a href="#" onclick="browser.switchSource('lastfm')"><img height="24px" src="images/lastfm.png"></a>
+<a href="#" onclick="browser.switchSource('slideshow')"><img height="24px" src="images/slideshow.png"></a>
+<a href="albumart.php" target="_blank"><img src="images/jewel-case-2.png" height="24px"></a>
+</td>
+<td align="right">
+<a href="#" onclick="expandInfo('right')"><img height="24px" id="expandright" src="images/arrow-right-double.png"></a>
+</td>
+</tr>
+</table>
