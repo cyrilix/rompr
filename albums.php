114749f656e68189a8cc80986611e38da27f7eb2
fatg3rman <fatg3rman@94fc339e-1912-44e7-90be-6e134bb66470>
2012-01-04 12:57:22 +0000

    git-svn-id: http://svn.code.sf.net/p/rompr/code/trunk@6 94fc339e-1912-44e7-90be-6e134bb66470

---
+++ albums.php
@@ -0,0 +1,26 @@
+<div style="padding-left:12px">
+<a href="#" onclick="toggleSearch()"><img class="topimg" height="20px" src="images/system-search.png"></a>
+</div>
+<div id="search" class="invisible">
+</div>
+
+<script language="javascript">
+$(document).ready(function(){
+    $('#search').load("search.php");
+});
+</script>
+
+<?php
+
+include ("vars.php");
+include ("functions.php");
+include ("connection.php");
+include ("collection.php");
+
+$count = 1;
+$divtype = "album1";
+$collection = doCollection("listallinfo");
+createHTML($collection->getSortedArtistList(), "a");
+close_mpd($connection);
+
+?>
