9e962cc65fe4d21915caac1ef61c7e94172fa3e0
fatg3rman <fatg3rman@94fc339e-1912-44e7-90be-6e134bb66470>
2012-07-15 16:19:17 +0000

    Adding file browser. This took multiple revisions because KDESVN decided it didn't want to add new files
    git-svn-id: http://svn.code.sf.net/p/rompr/code/trunk@79 94fc339e-1912-44e7-90be-6e134bb66470

---
+++ filesearch.php
@@ -0,0 +1,43 @@
+<?php
+include ("vars.php");
+include ("functions.php");
+include ("connection.php");
+include ("filelister.php");
+?>
+
+<form name="filesearch" action="filesearch.php" method="get">
+<ul class="sourcenav">
+<li><b>Search For Files Containing:</b></li>
+<li><input class="sourceform" name="searchtitle" type="text" size="60" />
+<button class="sourceform" type="submit">Search</button></li>
+</ul>
+</form>
+
+<?php
+
+$cmd = "";
+
+if(array_key_exists("searchtitle", $_REQUEST)) {
+    $cmd = 'search file "'.format_for_mpd(html_entity_decode($_REQUEST['searchtitle'])).'"';
+}
+
+if ($cmd != "") {
+    
+    $count = 1;
+    $tree = doFileList($cmd);
+    $tree->root->createHTML("b");
+    print '<div class="separator"></div>';
+
+}
+
+close_mpd($connection);
+
+?>
+
+<script type="text/javascript"> 
+
+$('form[name="filesearch"]').ajaxForm(function(data) { 
+    $('#filesearch').html(data);
+}); 
+
+</script>
