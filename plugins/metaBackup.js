203d6c9e62884866cfe788e8e1f08c8171488843
fatg3rman <fatg3rman@94fc339e-1912-44e7-90be-6e134bb66470>
2015-05-23 13:55:32 +0000

    Flippety Floppity Floo
    git-svn-id: http://svn.code.sf.net/p/rompr/code/trunk@531 94fc339e-1912-44e7-90be-6e134bb66470

---
+++ plugins/metaBackup.js
@@ -0,0 +1,215 @@
+var metaBackup = function() {
+	
+	var mbb = null;
+	var meta = new Array();
+	var progressbar;
+	var metaindex = 0;
+
+	function getBackupData() {
+		$("#mbbmunger").empty();
+        $.ajax({
+        	url: 'backends/sql/userRatings.php',
+        	type: "POST",
+        	data: {action: 'getbackupdata'},
+        	dataType: 'json',
+        	success: function(data) {
+        		metaBackup.doMainLayout(data);
+        	},
+        	error: function() {
+        		infobar.notify(infobar.ERROR, "Failed to get Backup info");
+        		mbb.slideToggle('fast');
+        	}
+        });
+	}
+
+	return {
+
+		open: function() {
+			if (mbb === null) {
+	        	mbb = browser.registerExtraPlugin("mbb", language.gettext("label_metabackup"), metaBackup);
+	        	if (prefs.apache_backend != 'sql') {
+		            $("#mbbfoldup").append('<h3 align="center">'+language.gettext("label_nosql")+'</h3>');
+		            $("#mbbfoldup").append('<h3 align="center"><a href="http://sourceforge.net/p/rompr/wiki/Enabling%20Rating%20and%20Tagging/" target="_blank">Read The Wiki</a></h3>');
+		            mbb.slideToggle('fast');
+		            return;
+	        	}
+
+    			$("#mbbfoldup").append('<div class="containerbox padright noselection">'+
+        			'<div class="expand">'+
+            		'</div>'+
+					'<button class="fixed" onclick="metaBackup.create()">'+language.gettext("button_backup")+'</button>'+
+    				'</div>');
+
+			    $("#mbbfoldup").append('<div class="noselection fullwidth" id="mbbmunger"></div>');
+			    getBackupData();
+			} else {
+				browser.goToPlugin("mbb");
+			}
+		},
+
+		doMainLayout: function(data) {
+			debug.log("METABACKUP",data);
+			meta = [];
+			var n = 0;
+			$("#mbbmunger").append('<h2>Existing Backup</h2>');
+			$("#mbbmunger").append('<div id="mbbrestore" class="containerbox padright noselection">'+
+    			'<div class="expand">'+
+        		'</div>'+
+				'<button id="ilikeboobs" class="fixed" onclick="metaBackup.restore()">'+language.gettext("button_restore")+'</button>'+
+				'</div>');
+            $("#mbbmunger").append(
+            	'<div name="minge" class="containerbox vertical">'+
+            		'<div style="margin-left:8px;margin-right:8px;margin-top:4px;margin-bottom:4px" class="containerbox">'+
+            			'<div class="fixed menuitem" style="width:10em"><b>Progress</b></div>'+
+            			'<div class="expand menuitem" id="metaprogress"></div>'+
+            		'</div>'+
+            	'</div>'
+            	);
+		    progressbar = new progressBar("metaprogress", "horizontal");
+
+			$('[name="minge"]').hide();
+
+			for (var r in data.Ratings) {
+				for (var t in data.Ratings[r]) {
+					n++;
+					var thing = {
+						action: "set",
+						attributes: [{
+							attribute: 'Rating',
+							value: r
+						}],
+						title: data.Ratings[r][t]['Title'],
+						artist: data.Ratings[r][t]['Artist'],
+						albumartist: data.Ratings[r][t]['Albumartist'],
+						album: data.Ratings[r][t]['Album'],
+						image: data.Ratings[r][t]['Image']
+					};
+					if (prefs.player_backend == "mopidy") {
+						var a = data.Ratings[r][t]['Uri'];
+						if (a) {
+							if (a.substr(0, 7) == "spotify" || a.substr(0,10) == "soundcloud") {
+								thing.uri = a;
+							}
+						}
+					}
+					meta.push(thing);
+				}
+			}
+			for (var r in data.Tags) {
+				for (var t in data.Tags[r]) {
+					n++;
+					var thing = {
+						action: "set",
+						attributes: [{
+							attribute: 'Tags',
+							value: [r]
+						}],
+						title: data.Tags[r][t]['Title'],
+						artist: data.Tags[r][t]['Artist'],
+						albumartist: data.Tags[r][t]['Albumartist'],
+						album: data.Tags[r][t]['Album'],
+						image: data.Tags[r][t]['Image']
+					};
+					if (prefs.player_backend == "mopidy") {
+						var a = data.Tags[r][t]['Uri'];
+						if (a) {
+							if (a.substr(0, 7) == "spotify" || a.substr(0,10) == "soundcloud") {
+								thing.uri = a;
+							}
+						}
+					}
+					meta.push(thing);
+				}
+			}
+			for (var r in data.Playcounts) {
+				n++;
+				var thing = {
+					action: "inc",
+					attributes: [{
+						attribute: 'Playcount',
+						value: data.Playcounts[r]['Playcount']
+					}],
+					title: data.Playcounts[r]['Title'],
+					artist: data.Playcounts[r]['Artist'],
+					albumartist: data.Playcounts[r]['Albumartist'],
+					album: data.Playcounts[r]['Album'],
+					image: data.Playcounts[r]['Image']
+				};
+				meta.push(thing);
+			}
+			if (n > 0) {
+				$("#mbbmunger").append('<table id="tits" align="center" cellpadding="2" width="95%" style="border-collapse:collapse"></table>');
+	            $("#tits").append('<tr><th>Title</th><th>Artist</th><th>Album</th><th>Album Artist</th><th>Metadata</th></tr>');
+	            for (var i in meta) {
+	            	var fuck = meta[i].attributes[0].attribute;
+	            	$("#tits").append('<tr><td>'+meta[i]['title']+'</td><td>'+meta[i]['artist']+'</td><td>'+meta[i]['album']+'</td><td>'+meta[i]['albumartist']+'</td><td>'+meta[i].attributes[0].attribute+' : '+getArray(meta[i].attributes[0].value)[0]+'</td></tr>');
+	            }
+	        }
+            if (!$("#mbbfoldup").is(':visible')) {
+	            mbb.slideToggle('fast', function() {
+		        	browser.goToPlugin("mbb");
+	            });
+	        }
+            if (n == 0) {
+            	$("#mbbrestore").fadeOut('fast');
+            }
+		},
+
+		create: function() {
+            $.ajax({
+            	url: 'backends/sql/userRatings.php',
+            	type: "POST",
+            	data: {action: 'metabackup'},
+            	success: function(data) {
+            		infobar.notify(infobar.NOTIFY, "Backup Created");
+            		getBackupData();
+            	},
+            	error: function() {
+            		infobar.notify(infobar.ERROR, "Failed to get Backup info");
+            		mbb.slideToggle('fast');
+            	}
+            });
+	    },
+
+		close: function() {
+			mbb = null;
+			meta = [];
+			metaindex = 0;
+		},
+
+		restore: function() {
+			debug.log("COCKWILLY","Cheeseburger");
+			$("#ilikeboobs").prop('disabled', true);
+			if (!$('[name="minge"]').is(':visible')) {
+				$('[name="minge"]').slideToggle('slow');
+			}
+			var p = (metaindex/meta.length)*100;
+			progressbar.setProgress(p.toFixed(2));
+			if (metaindex < meta.length) {
+				var arse = meta[metaindex];
+				debug.log("COCKWILLY","Restoring",arse);
+		        $.ajax({
+		            url: "backends/sql/userRatings.php",
+		            type: "POST",
+		            data: arse,
+		            dataType: 'json',
+		            success: function(rdata) {
+		                debug.log("FRIDGE","Success",rdata);
+		                updateCollectionDisplay(rdata);
+		                metaindex++;
+						$("#ilikeboobs").prop('disabled', false);
+					},
+		            error: function(rdata) {
+		                debug.warn("FRIDGE","Failure");
+		                metaindex++;
+						$("#ilikeboobs").prop('disabled', false);
+		            }
+		        });
+		    }
+		}
+
+	}
+
+}();
+
+pluginManager.addPlugin(language.gettext("label_metabackup"), metaBackup.open, null);
